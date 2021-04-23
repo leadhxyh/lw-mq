85ab82a248736def6dfba3ed3db0148162e526c4
leadhxyh <leadhxyh@gmail.com>
2021-04-23 18:32:28 +0800

    feat:加入启动类

---
+++ src/main/java/com/lw/mq/LwMqApplication.java
@@ -0,0 +1,13 @@
+package com.lw.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LwMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LwMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lw/mq/LwMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lw.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LwMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
